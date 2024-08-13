b17a853adc37f5124a5f2aba95e4fbd98cbd49fa
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-08-13 11:36:29 +0300

    cleared main

--- Assets/Beamable/Microservices/Service/Service.cs
+++ Assets/Beamable/Microservices/Service/Service.cs
@@ -8,25 +8,6 @@ namespace Beamable.Microservices
     [Microservice("Service")]
     public class Service : Microservice
     {
-        private static readonly Random Random = new Random();
-
-        [ClientCallable]
-        public async Task<string> CreateGroupWithSpaces(string groupName)
-        {
-            var groupNameDifferentiator = Random.Next(1000).ToString();
-            var fullGroupName = $"{groupName} {groupNameDifferentiator}";
-
-            try
-            {
-                var groupCreateRequest = new GroupCreateRequest(fullGroupName, null, "open", 0, 50);
-                var response = await Services.Groups.CreateGroup(groupCreateRequest);
-                var groupId = response.group.id.ToString();
-                return $"SUCCESS: Group '{fullGroupName}' created with ID: {groupId}";
-            }
-            catch (Exception e)
-            {
-                return $"ERROR: {e.Message}";
-            }
-        }
+        
     }
 }
