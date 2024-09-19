b36f763961b45815c7cb141411f8efafc276fecd
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-09-19 12:15:00 +0300

    delete inventory item refactor

--- Assets/Scripts/InventoryExample.cs
+++ Assets/Scripts/InventoryExample.cs
@@ -1,20 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Beamable;
 using UnityEngine;
+using UnityEngine.UI; // Required for UI elements
 
 public class InventoryExample : MonoBehaviour
 {
     private BeamContext _beamContext;
+
+    [SerializeField]
+    private List<string> inventoryItems = new List<string>(); 
+
+    [SerializeField]
+    private Button deleteButton;
 
     private async void Start()
     {
         _beamContext = await BeamContext.Default.Instance;
         Debug.Log($"Beamable initialized, PlayerId: {_beamContext.PlayerId}");
 
-        await GetInventory();
-        await DeleteItem();
+        deleteButton.onClick.AddListener(() => DeleteItem());
+
         await GetInventory();
     }
 
@@ -24,15 +30,18 @@ public class InventoryExample : MonoBehaviour
         var items = _beamContext.Inventory.GetItems();
 
         await items.Refresh();
+        inventoryItems.Clear(); // Clear previous items before updating
+
         foreach (var item in items)
         {
             Debug.Log($"Item id=[{item.ItemId}] type=[{item.ContentId}]");
+            inventoryItems.Add($"Item id=[{item.ItemId}], type=[{item.ContentId}]");
         }
 
         Debug.Log("Inventory fetched successfully.");
     }
 
-    public async Task DeleteItem()
+    public async void DeleteItem()
     {
         Debug.Log("Deleting an item...");
         var items = _beamContext.Inventory.GetItems();
@@ -43,6 +52,7 @@ public class InventoryExample : MonoBehaviour
             var itemToDelete = items[0];
             await _beamContext.Inventory.Update(builder => builder.DeleteItem(itemToDelete.ContentId, itemToDelete.ItemId));
             Debug.Log($"Deleted item id=[{itemToDelete.ItemId}] type=[{itemToDelete.ContentId}]");
+            await GetInventory();
         }
     }
 }
