4e2958fd4286e5bd413baa5c9c904f30967c6c1a
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-10-30 15:36:41 +0200

    Get invetory items

--- Assets/Scripts/InventoryExample.cs
+++ Assets/Scripts/InventoryExample.cs
@@ -2,29 +2,23 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Beamable;
 using UnityEngine;
-using UnityEngine.UI; // Required for UI elements
 
 public class InventoryExample : MonoBehaviour
 {
     private BeamContext _beamContext;
 
     [SerializeField]
-    private List<string> inventoryItems = new List<string>(); 
-
-    [SerializeField]
-    private Button deleteButton;
+    private List<string> inventoryItems; 
 
     private async void Start()
     {
         _beamContext = await BeamContext.Default.Instance;
         Debug.Log($"Beamable initialized, PlayerId: {_beamContext.PlayerId}");
-
-        deleteButton.onClick.AddListener(() => DeleteItem());
-
+        
         await GetInventory();
     }
 
-    public async Task GetInventory()
+    private async Task GetInventory()
     {
         Debug.Log("Fetching inventory...");
         var items = _beamContext.Inventory.GetItems();
@@ -41,18 +35,4 @@ public class InventoryExample : MonoBehaviour
         Debug.Log("Inventory fetched successfully.");
     }
 
-    public async void DeleteItem()
-    {
-        Debug.Log("Deleting an item...");
-        var items = _beamContext.Inventory.GetItems();
-        await items.Refresh();
-
-        if (items.Count > 0)
-        {
-            var itemToDelete = items[0];
-            await _beamContext.Inventory.Update(builder => builder.DeleteItem(itemToDelete.ContentId, itemToDelete.ItemId));
-            Debug.Log($"Deleted item id=[{itemToDelete.ItemId}] type=[{itemToDelete.ContentId}]");
-            await GetInventory();
-        }
-    }
 }
