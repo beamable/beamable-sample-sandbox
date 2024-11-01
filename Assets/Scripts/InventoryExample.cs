28b5eeacb96ac202f4e3d3e4ce3e4415144cfdcb
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-11-01 15:30:05 +0200

    Facebook sign in WIP

--- Assets/Scripts/InventoryExample.cs
+++ Assets/Scripts/InventoryExample.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Beamable;
+using Beamable.Common.Api.Inventory;
 using UnityEngine;
 
 public class InventoryExample : MonoBehaviour
@@ -8,13 +9,14 @@ public class InventoryExample : MonoBehaviour
     private BeamContext _beamContext;
 
     [SerializeField]
-    private List<string> inventoryItems; 
+    private List<string> inventoryItems;
 
     private async void Start()
     {
         _beamContext = await BeamContext.Default.Instance;
         Debug.Log($"Beamable initialized, PlayerId: {_beamContext.PlayerId}");
         
+
         await GetInventory();
     }
 
@@ -22,17 +24,52 @@ public class InventoryExample : MonoBehaviour
     {
         Debug.Log("Fetching inventory...");
         var items = _beamContext.Inventory.GetItems();
-
+    
         await items.Refresh();
-        inventoryItems.Clear(); // Clear previous items before updating
-
+        inventoryItems.Clear();
+    
         foreach (var item in items)
         {
             Debug.Log($"Item id=[{item.ItemId}] type=[{item.ContentId}]");
             inventoryItems.Add($"Item id=[{item.ItemId}], type=[{item.ContentId}]");
         }
-
+    
         Debug.Log("Inventory fetched successfully.");
     }
+    
+    // private async Task GetInventory()
+    // {
+    //     Debug.Log("Fetching inventory...");
+    //     var inventoryView = await _beamContext.Api.InventoryService.GetCurrent("");
+    //
+    //     foreach (var item in inventoryView.items)
+    //     {
+    //         List<ItemView> itemList = item.Value; // Access the List<ItemView> directly
+    //         foreach (var itemView in itemList)
+    //         {
+    //             Debug.Log($"Item id=[{itemView.id}] type=[{itemView.contentId}]");
+    //             inventoryItems.Add($"Item id=[{itemView.id}], type=[{itemView.contentId}]");
+    //         }
+    //     }
+    //
+    //     Debug.Log("Inventory fetched successfully.");
+    // }
 
+
+
+    private void AddToInventory(string itemRef, long amount, string itemAmountProperty)
+    {
+        var itemProperties = new Dictionary<string, string>
+        {
+            { itemAmountProperty, amount.ToString() }
+        };
+
+        _beamContext.Api.InventoryService.AddItem(itemRef, itemProperties).Then(_ =>
+        {
+            Debug.Log($"Success adding {amount} of {itemRef}");
+        }).Error(ex =>
+        {
+            Debug.LogError($"Error adding {amount} of {itemRef}: {ex.Message}");
+        });
+    }
 }
