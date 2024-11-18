952f2a2a1b32490f80c3269b7868ef30980bbca8
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-11-18 13:17:08 +0200

    get device id

--- Assets/Scripts/DeleteAccount.cs
+++ Assets/Scripts/DeleteAccount.cs
@@ -12,29 +12,9 @@ public class DeleteAccounts : MonoBehaviour
         _beamContext = await BeamContext.Default.Instance;
 
         Debug.Log($"Current PlayerId: {_beamContext.PlayerId}");
-        Debug.Log($"Token: {_beamContext.AccessToken.Token}");
-
-        // Get all player IDs from the microservice
-        var client = new ServiceClient();
-        var playerIds = await client.ListAllPlayers();
-
-        Debug.Log("Players to delete: " + string.Join(", ", playerIds));
-
-        // Delete each account
-        foreach (var playerId in playerIds)
-        {
-            Debug.Log(playerId);
-            try
-            {
-                var response = await client.DeleteAccount(playerId);
-                Debug.Log($"Deleted PlayerId: {playerId}, Response: {response}");
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"Failed to delete PlayerId: {playerId}, Error: {ex.Message}");
-            }
-        }
-
-        await Beam.ClearAndStopAllContexts();
+        Debug.Log($"Cid: {_beamContext.Cid}");
+        Debug.Log($"Pid: {_beamContext.Pid}");
+        var deviceID = SystemInfo.deviceUniqueIdentifier;
+        Debug.Log("Device Unique Identifier: " + deviceID);
     }
 }
