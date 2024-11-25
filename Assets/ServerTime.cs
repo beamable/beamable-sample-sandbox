103339bc0845f499d65e12caf33f85441426245d
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-11-25 13:44:14 +0200

    Get token

--- Assets/ServerTime.cs
+++ Assets/ServerTime.cs
@@ -1,45 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using Beamable;
 using Beamable.Api;
 using Beamable.Common.Api;
+using Beamable.Common.Dependencies;
+using UnityEditor;
 using UnityEngine;
 
 public class ServerTime : MonoBehaviour
 {
-    public long latestServerTimestamp;
-
-    private BeamContext ctx;
-    private IPlatformRequester _observer;
-
-    // Start is called before the first frame update
-    void Start()
+    private const string Prefix = "editor.0";
+    private const string Cid = "1774561898395648";
+    private const string Pid = "DE_1776244147362865";
+    
+    private static string PrefsKey(string thing)
     {
-        Debug.Log("Initializing BeamContext and IPlatformRequester...");
-        ctx = BeamContext.Default;
-
-        if (ctx == null)
-        {
-            Debug.LogError("BeamContext is not initialized. Ensure Beamable is set up correctly.");
-            return;
-        }
-
-        _observer = ctx.ServiceProvider.GetService<IPlatformRequester>();
-
-        if (_observer == null)
-        {
-            Debug.LogError("IPlatformRequester could not be retrieved. Check your Beamable setup.");
-        }
-        else
-        {
-            Debug.Log("IPlatformRequester successfully initialized.");
-        }
+    	return $"{Prefix}.{Cid}.{Pid}.{thing}";
     }
-
-    void Update()
+    
+    private static string CidPrefsKey(string thing)
     {
-
-            latestServerTimestamp = _observer.GetLatestServerTimestamp();
-
+    	return $"{Prefix}.{Cid}.{thing}";
+    }
+    
+    [MenuItem("DEBUG/Show Token Info")]
+    public static void ShowTokenExpiration()
+    {
+    	var accessToken = PlayerPrefs.GetString(CidPrefsKey("access_token"));
+    	var expires = PlayerPrefs.GetString(CidPrefsKey("expires"));
+    	var refreshToken = PlayerPrefs.GetString(CidPrefsKey("refresh_token"));
+    	var expiresDate = "invalid date";
+    	if (long.TryParse(expires, out var windowsFileTime))
+    	{
+    		expiresDate = DateTime.FromFileTimeUtc(windowsFileTime).ToString(CultureInfo.InvariantCulture);
+    	}
+    	Debug.Log($"Beamable token info: '{accessToken}' expiry {expires} ({expiresDate}) refresh='{refreshToken}'");
+    }
+    
+    [MenuItem("DEBUG/Force Expire Token")]
+    public static void ForceTokenExpiration()
+    {
+    	var expires = "0";
+    	var prefsKey = CidPrefsKey("expires");
+    	PlayerPrefs.SetString(prefsKey, expires);
+    	Debug.Log("Beamable token expiration set to epoch.");
+    }
+    
+    [MenuItem("DEBUG/Overwrite Access Token")]
+    public static void OverwriteAccessToken()
+    {
+    	var token = "6af40274-cc4b-4010-b85e-acde7dfa5777";
+    	var prefsKey = CidPrefsKey("access_token");
+    	PlayerPrefs.SetString(prefsKey, token);
+    	Debug.Log($"Overwrote access token with '{token}'");
     }
 }
